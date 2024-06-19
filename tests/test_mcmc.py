6d69147f46b439431352687c45da214bb5969022
david.randell <david.randell@shell.com>
2024-06-19 09:43:54 +0200

    renaming the fixture function for iteration. burn and thinning
    Signed-off-by: t.hajimohammadloo <t.hajimohammadloo@shell.com>
    Signed-off-by: david.randell <david.randell@shell.com>

--- tests/test_mcmc.py
+++ tests/test_mcmc.py
@@ -64,12 +64,10 @@ def fix_state(request):
 
 @pytest.fixture(
     params=[(0, 4000, 1), (2000, 4000, 5), (0, 6000, 10), (2000, 6000, 1)],
-    ids=[
-        "n_burn=0,n_iter=4000, n_thin=1",
-        "n_burn=non-zero,n_iter=4000,n_thin=5",
-        "n_burn=0,n_iter=6000, n_thin=10",
-        "n_burn=non-zero,n_iter=6000,n_thin=1",
-    ],
+    ids=["n_burn=0,n_iter=4000, n_thin=1",
+         "n_burn=non-zero,n_iter=4000,n_thin=5",
+         "n_burn=0,n_iter=6000, n_thin=10",
+         "n_burn=non-zero,n_iter=6000,n_thin=1"],
     name="mcmc_settings",
 )
 def fix_mcmc_settings(request):
@@ -121,10 +119,16 @@ def test_run_mcmc(state: dict, sampler: list, model: Model, mcmc_settings: dict,
     )
 =======
     M = MCMC(state, sampler, model,
+<<<<<<< HEAD
              n_burn=nburn_niter["nburn"],
              n_iter=nburn_niter["niter"],
              n_thin=nburn_niter["nthin"])
 >>>>>>> b4ba606 (updated unit test to include thinning)
+=======
+             n_burn=mcmc_settings["nburn"],
+             n_iter=mcmc_settings["niter"],
+             n_thin=mcmc_settings["nthin"])
+>>>>>>> 682a0d0 (renaming the fixture function for iteration. burn and thinning)
     M.store["count"] = 0
     M.run_mcmc()
     assert M.state["count"] == (M.n_iter + M.n_burn) * len(sampler) * M.n_thin
